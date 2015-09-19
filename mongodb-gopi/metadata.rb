14bd83fa9a67ba620387ae7772f5b570652c8c16
unknown <gnimmaga@gmail.com>
2015-09-19 11:55:05 -0400

    new file:   Berksfile 	new file:   mongodb-gopi/metadata.rb 	new file:   mongodb-gopi/recipes/default.rb

---
+++ mongodb-gopi/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-gopi"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "Chef Practise Inc"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
